4667b8dad34e2949757b069fcd6cfc13c38071df
ian larson <ian.larson@outlook.com>
2021-06-24 20:53:40 -0400

    Multiple variants basically working

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 
-.PHONY: all clean print
+.PHONY: all clean print release debug
+
+release:
+	@python3 buildo.py -m
 
 all:
-	@python3 buildo.py -m
+	@python3 buildo.py -m -t all
 
 clean:
 	@python3 buildo.py -t clean
+
+debug:
+	@python3 buildo.py -m -t debug
 
 print:
 	@python3 buildo.py -m -t print
--- buildo.py
+++ buildo.py
@@ -37,7 +37,7 @@ def simple_compile(compiler, src, obj, codegen, includedirs, libdirs, libs):
 
     cmd = [compiler, "-c", "-o", obj, src]
 
-    cmd.append(" ".join(codegen))
+    cmd.extend(codegen)
     cmd.extend([ f"-I{x}" for x in includedirs])
     cmd.extend([ f"-L{x}" for x in libdirs])
     cmd.extend([ f"-l{x}" for x in libs])
@@ -47,7 +47,7 @@ def simple_compile(compiler, src, obj, codegen, includedirs, libdirs, libs):
     subprocess.check_output(cmd)
 
 
-def do_task(g, compiler, finale, item):
+def do_task(g, compiler, item):
 
     # If the item was put into the graph with the directory timestamp function,
     # it's a directory.
@@ -55,25 +55,39 @@ def do_task(g, compiler, finale, item):
     if item_type == "directory":
         print(f"Making directory {item}")
         os.makedirs(item)
-    elif item_type == "object_file":
+    elif item_type == "release_object_file":
         # If the item is an object file, find the .c file in its predecessors and compile it
         # Find the c file
         cf = filter(lambda x: x.endswith(".c"), g.get_direct_predecessors(item))
         x = next(cf)
 
         simple_compile(compiler, x, item, ["-Ofast"], ["inc"], [], [])
-    elif item_type == "main_output":
+    elif item_type == "debug_object_file":
+        # If the item is an object file, find the .c file in its predecessors and compile it
+        # Find the c file
+        cf = filter(lambda x: x.endswith(".c"), g.get_direct_predecessors(item))
+        x = next(cf)
+
+        simple_compile(compiler, x, item, ["-Og", "-ggdb3"], ["inc"], [], [])
+    elif item_type == "release_main_output":
         # Otherwise its the final output. Find all the object files that go
         # into it and compile it.
         of = filter(lambda x: x.endswith(".o"), g.get_direct_predecessors(item))
-        final_cmd = [compiler] + list(of) + ["-o", finale]
+        final_cmd = [compiler] + list(of) + ["-o", item]
+        print(final_cmd)
+        subprocess.check_output(final_cmd)
+    elif item_type == "debug_main_output":
+        # Otherwise its the final output. Find all the object files that go
+        # into it and compile it.
+        of = filter(lambda x: x.endswith(".o"), g.get_direct_predecessors(item))
+        final_cmd = [compiler] + list(of) + ["-o", item, "-Og", "-ggdb3"]
         print(final_cmd)
         subprocess.check_output(final_cmd)
     else:
         print(f"Unexpected item type for {item}")
 
 
-def tw(w, g, tsd, finale):
+def tw(w, g, tsd):
 
     cc = tsd["cc"]
     while True:
@@ -89,7 +103,7 @@ def tw(w, g, tsd, finale):
             break
 
         try:
-            do_task(g, cc, finale, item)
+            do_task(g, cc, item)
             w.mark_done(item)
         except Exception as e:
             print(e)
@@ -126,17 +140,10 @@ def do_main():
             except:
                 pass
             return
+        pass
+
     # Create the graph, add the output directory(ies)
     g = Graph()
-    g.add_vertex("out", "directory")
-
-    # Add the final binary name
-    finale = os.path.join("out", "collect")
-    g.add_vertex(finale, "main_output")
-
-    g.add_vertex(tsd.name("cc"), "tsd_entry")
-
-    g.add_edge(finale, tsd.name("cc"))
 
     # Find all the headers and c source files
     cfiles = glob.glob("**/*.c")
@@ -145,42 +152,70 @@ def do_main():
         # Add all the headers to the graph
         g.add_vertex(f, "file")
 
+    g.add_vertex(tsd.name("cc"), "tsd_entry")
+
     mkdeps = []
     # Run makedeps on each of the source files with the inc dir
     with ThreadPoolExecutor() as executor:
         mkdeps = executor.map(lambda x:(x, *makedeps(x, "inc", False, tsd["cc"])), cfiles)
 
-    for f, o, deps in mkdeps:
-        # Add all the c source files to the graph
-        g.add_vertex(f, "file")
-        # Run makedeps on each of the source files with the inc dir
-        #o, deps = cc.makedeps(f, "inc")
+    mkdeps = list(mkdeps)
 
-        # Add the object file to the graph
-        ofile = os.path.join("out", o)
-        g.add_vertex(ofile, "object_file")
+    finales = []
+    variants = ["release"]
+    if "debug" in targets:
+        variants = ["debug"]
+    if "all" in targets:
+        variants = ["debug", "release"]
 
-        # Add edges from `out/` to the object file
-        g.add_edge(ofile, "out")
-        # Headers + c file to object file
-        g.add_edges(ofile, deps)
+    for v in variants:
+        ov = os.path.join("out", v)
+        g.add_vertex(ov, "directory")
 
-        g.add_edge(ofile, tsd.name("cc"))
-        # object file to final output
-        g.add_edge(finale, ofile)
+        # Add the final binary name
+        finale = os.path.join(ov, "collect")
+        finales.append(finale)
+        g.add_vertex(finale, v + "_main_output")
+
+        g.add_edge(finale, tsd.name("cc"))
+        for f, o, deps in mkdeps:
+            # Add all the c source files to the graph
+            if f not in g:
+                g.add_vertex(f, "file")
+            # Run makedeps on each of the source files with the inc dir
+            #o, deps = cc.makedeps(f, "inc")
+
+            # Add the object file to the graph
+            ofile = os.path.join(ov, o)
+            g.add_vertex(ofile, v + "_object_file")
+
+            # Add edges from `out/` to the object file
+            g.add_edge(ofile, ov)
+            # Headers + c file to object file
+            g.add_edges(ofile, deps)
+
+            g.add_edge(ofile, tsd.name("cc"))
+            # object file to final output
+            g.add_edge(finale, ofile)
+
+        pass
+
 
     ts_tsd_entry = lambda x : tsd.time(x)
     func_dict = {
         "file" : ts_file,
         "directory" : ts_directory,
-        "main_output" : ts_file,
-        "object_file" : ts_file,
+        "release_main_output" : ts_file,
+        "debug_main_output" : ts_file,
+        "release_object_file" : ts_file,
+        "debug_object_file" : ts_file,
         "tsd_entry" :  ts_tsd_entry,
     }
 
     # Create a work queue with the end goal of the finale file
     w = WorkQueue(g, func_dict)
-    w.activate(finale)
+    for f in finales:
+        w.activate(f)
 
     if "print" in targets:
         for item in w.get_updated():
@@ -192,7 +227,7 @@ def do_main():
 
     # Create and start some threads
     for i in range(nthreads):
-        t = threading.Thread(target=tw, args=(w, g, tsd, finale))
+        t = threading.Thread(target=tw, args=(w, g, tsd))
         threads.append(t)
         t.start()
 
